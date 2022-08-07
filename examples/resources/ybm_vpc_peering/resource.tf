fb087fda7e18b81672bcb1800e8d823df0a10cee
Srinivas Pothuraju <posriniv@gmail.com>
2022-08-07 12:07:48 -0500

    update vpc peering documentation

--- examples/resources/ybm_vpc_peering/resource.tf
+++ examples/resources/ybm_vpc_peering/resource.tf
@@ -1,12 +1,26 @@
+#AWS VPC Peering
+
 resource "ybm_vpc_peering" "example_vpc_peering" {
   account_id = var.account_id
   name = "example_name"
   yugabytedb_vpc_id = "example_vpc_id"
   application_vpc_info = {
-    cloud_type = "GCP"
-    cloud_project = "example_project"
-    cloud_region = "us-west1"
+    cloud = "AWS"
+    project = "example_project"
+    region = "us-west1"
     vpc_id = "application_vpc_id"
     cidr = "example_cidr"
+  }
+}
+
+#GCP VPC Peering
+resource "ybm_vpc_peering" "example_vpc_peering" {
+  account_id = var.account_id
+  name = "example_name"
+  yugabytedb_vpc_id = "example_vpc_id"
+  application_vpc_info = {
+    cloud = "GCP"
+    project = "example_project"
+    vpc_id = "application_vpc_id"
   }
 }
