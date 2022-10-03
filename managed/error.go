607c9a523440d7a128da1610875bee93ba0b0ba6
Srinivas Pothuraju <posriniv@gmail.com>
2022-10-03 13:34:18 -0500

    handle dump error

--- managed/error.go
+++ managed/error.go
@@ -7,8 +7,12 @@ import (
 
 func getErrorMessage(response *http.Response, err error) string {
 	errMsg := []byte(err.Error())
+	var dumpErr error
 	if response != nil {
-		errMsg, _ = httputil.DumpResponse(response, true)
+		errMsg, dumpErr = httputil.DumpResponse(response, true)
+		if dumpErr != nil {
+			errMsg = []byte("Error while dumping response: " + dumpErr.Error())
+		}
 	}
 	return string(errMsg)
 }
