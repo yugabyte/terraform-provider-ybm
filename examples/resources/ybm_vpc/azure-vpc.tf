a3825a3d635aceef8fac2f26bd06e38dd2ba8de9
cdavid <catalindavid2003@gmail.com>
2023-06-29 12:26:21 -0700

    Update documentation

---
+++ examples/resources/ybm_vpc/azure-vpc.tf
@@ -0,0 +1,11 @@
+resource "ybm_vpc" "example-vpc" {
+  name = "example-vpc"
+  cloud = "AZURE"
+  region_cidr_info = [
+    {
+      region = "eastus"
+      # For Azure, the CIDR is auto-assigned
+      # cidr = "10.231.0.0/24"
+    }
+  ]
+}
