226ed1b43139dd1b5588fbd6de539eb57d1f404e
Srinivas Pothuraju <posriniv@gmail.com>
2024-01-29 23:28:09 -0600

    add support to change default value of the feature flag

--- managed/fflags/feature_flags.go
+++ managed/fflags/feature_flags.go
@@ -16,11 +16,15 @@ const (
 	INCREMENTAL_BACKUP FeatureFlag = "INCREMENTAL_BACKUP"
 )
 
+var flagEnabled = map[FeatureFlag]bool{
+	INCREMENTAL_BACKUP: false,
+}
+
 func (f FeatureFlag) String() string {
 	return string(f)
 }
 
 func IsFeatureFlagEnabled(featureFlag FeatureFlag) bool {
 	envVarName := "YBM_FF_" + featureFlag.String()
-	return strings.ToLower(os.Getenv(envVarName)) == "true"
+	return strings.ToLower(os.Getenv(envVarName)) == "true" || flagEnabled[INCREMENTAL_BACKUP]
 }
