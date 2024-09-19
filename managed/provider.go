e201103e65c418d1a534338914967dbceb492679
Siddarth-Baldwa <siddarthbaldwa@gmail.com>
2024-09-19 07:37:28 +0000

    Add ybm_integration data source

--- managed/provider.go
+++ managed/provider.go
@@ -162,9 +162,10 @@ func (p *provider) GetResources(_ context.Context) (map[string]tfsdk.ResourceTyp
 
 func (p *provider) GetDataSources(_ context.Context) (map[string]tfsdk.DataSourceType, diag.Diagnostics) {
 	return map[string]tfsdk.DataSourceType{
-		"ybm_backup":     dataSourceBackupType{},
-		"ybm_cluster":    dataClusterNameType{},
-		"ybm_vpc":        dataSourceVPCType{},
-		"ybm_allow_list": dataSourceAllowListType{},
+		"ybm_backup":      dataSourceBackupType{},
+		"ybm_cluster":     dataClusterNameType{},
+		"ybm_vpc":         dataSourceVPCType{},
+		"ybm_allow_list":  dataSourceAllowListType{},
+		"ybm_integration": dataSourceIntegrationType{},
 	}, nil
 }
