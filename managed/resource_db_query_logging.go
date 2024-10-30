4df5a26793701a61553d748bc0e4927394a85ab0
bhupendray-yb <bhupendra.yadav@yugabyte.com>
2024-10-30 13:27:37 +0530

    Added support for db query logging update & delete

--- managed/resource_db_query_logging.go
+++ managed/resource_db_query_logging.go
@@ -60,8 +60,7 @@ func (r resourceDbQueryLoggingType) GetSchema(ctx context.Context) (tfsdk.Schema
 			},
 			"log_config": {
 				Description: "The Log config.",
-				Optional:    true,
-				Computed:    true,
+				Required:    true,
 				Attributes: tfsdk.SingleNestedAttributes(map[string]tfsdk.Attribute{
 					"log_min_duration_statement": {
 						Description: "Duration(in ms) of each completed statement to be logged if the statement ran for at least the specified amount of time.",
@@ -144,15 +143,28 @@ func getConfigFromPlan(ctx context.Context, plan tfsdk.Plan, config *DbQueryLogg
 	return diags
 }
 
-func buildDbQueryLoggingSpec(config DbQueryLoggingConfig, integrationId string) (*openapiclient.PgLogExporterConfigSpec, error) {
-	// Start with a default log config
-	newLogConfig := openapiclient.NewPgLogExportConfigWithDefaults()
+func getConfigFromState(ctx context.Context, state tfsdk.State, config *DbQueryLoggingConfig) {
+	state.GetAttribute(ctx, path.Root("account_id"), &config.AccountID)
+	state.GetAttribute(ctx, path.Root("project_id"), &config.ProjectID)
+	state.GetAttribute(ctx, path.Root("cluster_name"), &config.ClusterName)
+	state.GetAttribute(ctx, path.Root("cluster_id"), &config.ClusterID)
+	state.GetAttribute(ctx, path.Root("integration_name"), &config.IntegrationName)
+	state.GetAttribute(ctx, path.Root("config_id"), &config.ConfigID)
+	state.GetAttribute(ctx, path.Root("log_config"), &config.LogConfig)
+}
+
+func buildDbQueryLoggingSpec(config DbQueryLoggingConfig, integrationId string, exportConfig *openapiclient.PgLogExportConfig) (*openapiclient.PgLogExporterConfigSpec, error) {
+	newLogConfig := *exportConfig
 
 	// Update log config params, that are provided in tf file
 	if config.LogConfig != nil {
 		tfLogConfig := *config.LogConfig
 		if !tfLogConfig.LogConnections.IsNull() && !tfLogConfig.LogConnections.IsUnknown() {
 			newLogConfig.SetLogConnections(tfLogConfig.LogConnections.Value)
+		}
+
+		if !tfLogConfig.DebugPrintPlan.IsNull() && !tfLogConfig.DebugPrintPlan.IsUnknown() {
+			newLogConfig.SetDebugPrintPlan(tfLogConfig.DebugPrintPlan.Value)
 		}
 
 		if !tfLogConfig.LogDisconnections.IsNull() && !tfLogConfig.LogDisconnections.IsUnknown() {
@@ -198,9 +210,37 @@ func buildDbQueryLoggingSpec(config DbQueryLoggingConfig, integrationId string) 
 
 	// Return the new PgLogExporterConfigSpec
 	return &openapiclient.PgLogExporterConfigSpec{
-		ExportConfig: *newLogConfig,
+		ExportConfig: newLogConfig,
 		ExporterId:   integrationId,
 	}, nil
+}
+
+func getPgLogExporterConfig(ctx context.Context, accountId string, projectId string, clusterId string, apiClient *openapiclient.APIClient) (conf *openapiclient.PgLogExporterConfigData, ok bool, errMsg string) {
+	specList, _, err := apiClient.ClusterApi.ListPgLogExporterConfigs(ctx, accountId, projectId, clusterId).Execute()
+	if err != nil {
+		return nil, false, GetApiErrorDetails(err)
+	}
+
+	if len(specList.GetData()) < 1 {
+		return nil, false, "no DB query logging config found for the cluster"
+	}
+	return &specList.Data[0], true, ""
+}
+
+func getIntegrationId(ctx context.Context, accountId string, projectId string, integrationName string, apiClient *openapiclient.APIClient) (data *openapiclient.TelemetryProviderData, ok bool, errMsg string) {
+	integrationConfig, _, err := apiClient.TelemetryProviderApi.
+		ListTelemetryProviders(ctx, accountId, projectId).
+		Name(integrationName).
+		Execute()
+	if err != nil {
+		return data, false, GetApiErrorDetails(err)
+	}
+
+	if len(integrationConfig.GetData()) < 1 {
+		errMsg := fmt.Sprintf("Integration %s not found", integrationName)
+		return nil, false, errMsg
+	}
+	return &integrationConfig.GetData()[0], true, ""
 }
 
 // Read latest state/config of a resource from Backend and convert it to model
@@ -208,17 +248,10 @@ func resourceRead(ctx context.Context, accountId string, projectId string, clust
 	clusterName string, integrationName string,
 	apiClient *openapiclient.APIClient) (dbQueryLoggingConfig DbQueryLoggingConfig, readOK bool, errMsg string) {
 
-	// fetch log config from backend
-	specList, _, err := apiClient.ClusterApi.ListPgLogExporterConfigs(ctx, accountId, projectId, clusterId).Execute()
-	if err != nil {
-		return dbQueryLoggingConfig, false, GetApiErrorDetails(err)
+	spec, ok, errMsg := getPgLogExporterConfig(ctx, accountId, projectId, clusterId, apiClient)
+	if !ok {
+		return dbQueryLoggingConfig, false, errMsg
 	}
-
-	if len(specList.GetData()) == 0 {
-		return dbQueryLoggingConfig, false, fmt.Sprintf("No DB query logging config found for cluster %s", clusterName)
-	}
-
-	spec := specList.Data[0]
 	exportConfig := spec.Spec.ExportConfig
 
 	// Initialize the LogConfig object from PgLogExportConfig
@@ -248,18 +281,8 @@ func resourceRead(ctx context.Context, accountId string, projectId string, clust
 	return dbQueryLoggingConfig, true, ""
 }
 
-func getConfigFromState(ctx context.Context, state tfsdk.State, config *DbQueryLoggingConfig) {
-	state.GetAttribute(ctx, path.Root("account_id"), &config.AccountID)
-	state.GetAttribute(ctx, path.Root("project_id"), &config.ProjectID)
-	state.GetAttribute(ctx, path.Root("cluster_name"), &config.ClusterName)
-	state.GetAttribute(ctx, path.Root("cluster_id"), &config.ClusterID)
-	state.GetAttribute(ctx, path.Root("integration_name"), &config.IntegrationName)
-	state.GetAttribute(ctx, path.Root("config_id"), &config.ConfigID)
-}
-
 func (r resourceDbQueryLogging) Read(ctx context.Context, req tfsdk.ReadResourceRequest, resp *tfsdk.ReadResourceResponse) {
 	var state DbQueryLoggingConfig
-
 	getConfigFromState(ctx, req.State, &state)
 	apiClient := r.p.client
 	accountId := state.AccountID.Value
@@ -282,17 +305,137 @@ func (r resourceDbQueryLogging) Read(ctx context.Context, req tfsdk.ReadResource
 }
 
 func (r resourceDbQueryLogging) Update(ctx context.Context, req tfsdk.UpdateResourceRequest, resp *tfsdk.UpdateResourceResponse) {
-	resp.Diagnostics.AddError(
-		"Unsupported Operation",
-		"Update is not currently supported.",
-	)
+	var planConfig DbQueryLoggingConfig
+	resp.Diagnostics.Append(getConfigFromPlan(ctx, req.Plan, &planConfig)...)
+	if resp.Diagnostics.HasError() {
+		tflog.Debug(ctx, "Error while getting the plan for the Db query logging config")
+		return
+	}
+	integrationName := planConfig.IntegrationName.Value
+
+	apiClient := r.p.client
+	var stateConfig DbQueryLoggingConfig
+	getConfigFromState(ctx, req.State, &stateConfig)
+	accountId := stateConfig.AccountID.Value
+	projectId := stateConfig.ProjectID.Value
+	clusterId := stateConfig.ClusterID.Value
+	clusterName := stateConfig.ClusterName.Value
+	configId := stateConfig.ConfigID.Value
+
+	if planConfig.ClusterName != stateConfig.ClusterName {
+		errMsg := "Field cluster_name cannot be changed after resource creation"
+		resp.Diagnostics.AddError(errMsg, errMsg)
+		return
+	}
+
+	spec, ok, errMsg := getPgLogExporterConfig(ctx, accountId, projectId, clusterId, apiClient)
+	if !ok {
+		resp.Diagnostics.AddError("Unable to fetch DB query logging config", errMsg)
+		return
+	}
+
+	integrationId := ""
+	if planConfig.IntegrationName != stateConfig.IntegrationName {
+		integrationConfig, ok, errMsg := getIntegrationId(ctx, accountId, projectId, integrationName, apiClient)
+		if !ok {
+			resp.Diagnostics.AddError("Unable to fetch integration details for: "+integrationName, errMsg)
+			return
+		}
+		integrationId = integrationConfig.Info.Id
+	} else {
+		integrationId = spec.Spec.ExporterId
+	}
+	// Use planConfig provided in tf file to build new API Pg log exporter config spec
+	apiConfigSpec, err := buildDbQueryLoggingSpec(planConfig, integrationId, &spec.Spec.ExportConfig)
+	if err != nil {
+		resp.Diagnostics.AddError("Unable to update DB query logging config", GetApiErrorDetails(err))
+		return
+	}
+
+	_, _, err = apiClient.ClusterApi.UpdatePgLogExporterConfig(ctx, accountId, projectId, clusterId, configId).PgLogExporterConfigSpec(*apiConfigSpec).Execute()
+	if err != nil {
+		resp.Diagnostics.AddError(fmt.Sprintf("Unable to update DB query logging config for cluster %s", clusterName), GetApiErrorDetails(err))
+		return
+	}
+
+	retryPolicy := retry.NewConstant(10 * time.Second)
+	retryPolicy = retry.WithMaxDuration(2400*time.Second, retryPolicy)
+	err = retry.Do(ctx, retryPolicy, func(ctx context.Context) error {
+		asState, readInfoOK, message := getTaskState(accountId, projectId, clusterId, openapiclient.ENTITYTYPEENUM_CLUSTER, openapiclient.TASKTYPEENUM_EDIT_DATABASE_QUERY_LOGGING, apiClient, ctx)
+		if readInfoOK {
+			if asState == string(openapiclient.TASKACTIONSTATEENUM_SUCCEEDED) {
+				return nil
+			}
+			if asState == string(openapiclient.TASKACTIONSTATEENUM_FAILED) {
+				return fmt.Errorf("failed to update DB query log config")
+			}
+		} else {
+			return retry.RetryableError(errors.New("unable to check DB query log config update status: " + message))
+		}
+		return retry.RetryableError(errors.New("DB query log config is being updated"))
+	})
+
+	if err != nil {
+		errorSummary := fmt.Sprintf("Unable to update DB query log config for cluster: %s", clusterId)
+		resp.Diagnostics.AddError(errorSummary, "The operation timed out waiting for DB query log config update operation.")
+		return
+	}
+
+	planConfig.ConfigID.Value = configId
+
+	dbqlConfig, readOK, readErrMsg := resourceRead(ctx, accountId, projectId, clusterId,
+		clusterName, integrationName, apiClient)
+	if !readOK {
+		resp.Diagnostics.AddError("Unable to read the state of Db Query log config ", readErrMsg)
+		return
+	}
+
+	diags := resp.State.Set(ctx, &dbqlConfig)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
 }
 
 func (r resourceDbQueryLogging) Delete(ctx context.Context, req tfsdk.DeleteResourceRequest, resp *tfsdk.DeleteResourceResponse) {
-	resp.Diagnostics.AddError(
-		"Unsupported Operation",
-		"Delete is not currently supported.",
-	)
+	apiClient := r.p.client
+	var state DbQueryLoggingConfig
+	getConfigFromState(ctx, req.State, &state)
+	accountId := state.AccountID.Value
+	projectId := state.ProjectID.Value
+	clusterId := state.ClusterID.Value
+	configId := state.ConfigID.Value
+	clusterName := state.ClusterName.Value
+
+	_, err := apiClient.ClusterApi.RemovePgLogExporterConfig(ctx, accountId, projectId, clusterId, configId).Execute()
+	if err != nil {
+		resp.Diagnostics.AddError(fmt.Sprintf("Unable to remove DB query logging config for cluster: %s", clusterName), GetApiErrorDetails(err))
+		return
+	}
+
+	retryPolicy := retry.NewConstant(10 * time.Second)
+	retryPolicy = retry.WithMaxDuration(2400*time.Second, retryPolicy)
+	err = retry.Do(ctx, retryPolicy, func(ctx context.Context) error {
+		asState, readInfoOK, message := getTaskState(accountId, projectId, clusterId, openapiclient.ENTITYTYPEENUM_CLUSTER, openapiclient.TASKTYPEENUM_DISABLE_DATABASE_QUERY_LOGGING, apiClient, ctx)
+		if readInfoOK {
+			if asState == string(openapiclient.TASKACTIONSTATEENUM_SUCCEEDED) {
+				return nil
+			}
+			if asState == string(openapiclient.TASKACTIONSTATEENUM_FAILED) {
+				return ErrFailedTask
+			}
+		} else {
+			return retry.RetryableError(errors.New("Unable to check status for DB query logging removal task: " + message))
+		}
+		return retry.RetryableError(errors.New("DB Query log configuration is being removed from the cluster"))
+	})
+
+	if err != nil {
+		resp.Diagnostics.AddError("Unable to remove Db Logging config from the cluster ", "The operation timed out waiting for DB  Query Logging removal to complete.")
+		return
+	}
+
+	resp.State.RemoveResource(ctx)
 }
 
 func (r resourceDbQueryLogging) ImportState(ctx context.Context, req tfsdk.ImportResourceStateRequest, resp *tfsdk.ImportResourceStateResponse) {
@@ -343,26 +486,16 @@ func (r resourceDbQueryLogging) Create(ctx context.Context, req tfsdk.CreateReso
 	}
 	clusterId = clusterData.Info.Id
 
-	integrationConfig, _, err := apiClient.TelemetryProviderApi.
-		ListTelemetryProviders(context.Background(), accountId, projectId).
-		Name(integrationName).
-		Execute()
-
-	if err != nil {
-		resp.Diagnostics.AddError("Unable to fetch integration details for integration: "+integrationName, GetApiErrorDetails(err))
+	integrationConfig, ok, errMsg := getIntegrationId(ctx, accountId, projectId, integrationName, apiClient)
+	if !ok {
+		resp.Diagnostics.AddError("Unable to fetch integration details for: "+integrationName, errMsg)
 		return
 	}
+	integrationId = integrationConfig.Info.Id
 
-	if len(integrationConfig.GetData()) < 1 {
-		message := fmt.Sprintf("Integration %s not found", integrationName)
-		resp.Diagnostics.AddError(message, message)
-		return
-	}
-	integrationId = integrationConfig.GetData()[0].Info.Id
-
-	dbQueryLoggingConfigSpec, err := buildDbQueryLoggingSpec(config, integrationId)
+	dbQueryLoggingConfigSpec, err := buildDbQueryLoggingSpec(config, integrationId, openapiclient.NewPgLogExportConfigWithDefaults())
 	if err != nil {
-		resp.Diagnostics.AddWarning("Unable to build DB query logging config spec", GetApiErrorDetails(err))
+		tflog.Warn(ctx, "Unable to build DB query logging config spec"+GetApiErrorDetails(err))
 		resp.Diagnostics.AddError("Encountered error while enabling DB Query Logging", GetApiErrorDetails(err))
 		return
 	}
