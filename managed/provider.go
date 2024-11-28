2fe3de4c65b29711930eadd3c09dda1424692845
Srinivas Pothuraju <posriniv@gmail.com>
2024-11-28 07:37:00 -0600

    support DR in TF provider

--- managed/provider.go
+++ managed/provider.go
@@ -165,6 +165,16 @@ func (p *provider) GetResources(_ context.Context) (map[string]tfsdk.ResourceTyp
 		resources["ybm_db_audit_logging"] = resourceDbAuditLoggingType{}
 	}
 
+	// Add DB Query logging resource only if the feature flag is enabled
+	if fflags.IsFeatureFlagEnabled(fflags.DB_QUERY_LOGGING) {
+		resources["ybm_db_query_logging"] = resourceDbQueryLoggingType{}
+	}
+
+	// Add DR config resource only if the feature flag is enabled
+	if fflags.IsFeatureFlagEnabled(fflags.DR) {
+		resources["ybm_dr_config"] = resourceDrConfigType{}
+	}
+
 	return resources, nil
 }
 
